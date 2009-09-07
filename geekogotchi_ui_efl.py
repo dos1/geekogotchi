002cf680178523f142c61684cc29c4b41566111f
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-09-08 00:39:07 +0200

    More basic files

--- geekogotchi_ui_efl.py
+++ geekogotchi_ui_efl.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
-import elementary
+import elementary, edje, e_dbus
 
 class GeekogotchiUI_EFL:
   parent = None
   def __init__(self, p):
     self.parent = p
     elementary.init()
+    self.win = elementary.Window('geekogotchi', elementary.ELM_WIN_BASIC)
+    self.win.title_set('Geekogotchi')
+    bg = elementary.Background(self.win)
+    bg.show()
+    self.win.resize_object_add(bg)
+    self.win.destroy = self.windowClose
+  def windowClose(self, *args, **kargs):
+    self.parent.stop()
+  def connect(self):
+    self.parent.connect(mainloop = e_dbus.DBusEcoreMainLoop())
   def start(self):
     '''Start UI, mainloop etc.'''
-    self.parent.connect()
+    self.connect()
+    self.win.show()
     elementary.run()
   def exit(self):
     '''Exit from UI mainloop'''
@@ -17,4 +28,4 @@ class GeekogotchiUI_EFL:
   def destroy(self):
     '''Shutting down client'''
     self.parent.disconnect()
-
+    elementary.shutdown()
